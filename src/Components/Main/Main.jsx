84a2ffaafe57425e204aa8fdad9003b7575a4859
dishaagrawalcodes <dishaagrawal9568@gmail.com>
2025-01-23 22:29:40 +0530

    added main.jsx

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+const Main = () => {
+  return (
+    
+      <div className="main">
+        <div className="nav">
+            <p>Gemini</p>
+            <img src={assets.user_icon} alt=""></img>
+        </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello,Dev.</span></p>
+                <p>How can i help you today?</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                    <img src={assets.compass_icon} alt=""></img>
+                </div>
+                <div className="card">
+                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                    <img src={assets.bulb_icon} alt=""></img>
+                </div>
+                <div className="card">
+                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                    <img src={assets.message_icon} alt=""></img>
+                </div>
+                <div className="card">
+                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                    <img src={assets.code_icon} alt=""></img>
+                </div>
+               
+            </div>
+        </div>
+      </div>
+   
+  )
+}
+
+export default Main
