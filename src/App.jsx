2dc4a2522a4ec32e915cbaa04c56251581688b0a
dishaagrawalcodes <dishaagrawal9568@gmail.com>
2025-01-29 19:58:37 +0530

    added contextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import ContextProvider from "./context/Context"
 import Sidebar from './Components/Sidebar/Sidebar'
 import Main from'./Components/Main/Main'
 const App = () => {
+  const handleSent = (prompt) => {
+    console.log("Prompt Sent:", prompt);
+  };
+
   return (
-    <>
-      <Sidebar/>
+    <ContextProvider>
+      <Sidebar onSent={handleSent}/>
       <Main/>
-      </>
+      </ContextProvider>
   )
 }
 
