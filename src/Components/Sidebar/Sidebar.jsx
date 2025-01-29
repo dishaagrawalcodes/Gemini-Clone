435db32c4a4181897505bf6a5176c052173f3e5f
dishaagrawalcodes <dishaagrawal9568@gmail.com>
2025-01-29 19:59:19 +0530

    promptclick directly sends

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -3,14 +3,10 @@ import "./Sidebar.css";
 import { assets } from "../../assets/assets";
 import { Context } from "../../context/Context";
 
-const Sidebar = ({onSent}) => {
+const Sidebar = ({ onSent }) => {
+  console.log(onSent);
   const [extended, setExtended] = useState(false);
-  const { setInput, prevPrompt, clearPrompts } = useContext(Context);
-  const handlePreviousPromptClick = (prompt) => {
-    setInput(prompt); // Set the selected prompt as the input
-    onSent(prompt); // Immediately send the selected prompt
-  };
-  
+  const { setInput, prevPrompt,clearSpecificPrompt, handleRecentPromptClick } = useContext(Context);
   return (
     <div className="sidebar">
       <div className="top">
@@ -27,20 +23,25 @@ const Sidebar = ({onSent}) => {
         {extended ? (
           <div className="recent">
             <p className="recent-title">Recent</p>
-            {Array.isArray(prevPrompt) && prevPrompt.length > 0 ? (
-              prevPrompt.map((item, index) => (
-                <div key={index} className="recent-entry"  onClick={() => handlePreviousPromptClick(item)}>
-                  <img src={assets.message_icon} alt="message" />
-                  <p>
-                    {typeof item === "string"
-                      ? `${item.slice(0, 18)}...`
-                      : `${JSON.stringify(item).slice(0, 18)}...`}
-                  </p>
-                </div>
-              ))
-            ) : (
-             null
-            )}
+            {Array.isArray(prevPrompt) && prevPrompt.length > 0
+              ? prevPrompt.map((item, index) => (
+                  <div
+                    key={index}
+                    className="recent-entry"
+                    onClick={() => handleRecentPromptClick(item?.prompt || item)}
+                  >
+                    <img src={assets.message_icon} alt="message" />
+                    
+                    <p>
+                      {item?.prompt
+                        ? `${item.prompt.slice(0, 18)}...`
+                        : "Unknown"}
+                         <button onClick={(e) => {
+                          e.stopPropagation();clearSpecificPrompt(item)}}>Clear</button>
+                    </p>
+                  </div>
+                ))
+              : null}
           </div>
         ) : null}
       </div>
